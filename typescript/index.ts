25ee23c2eddfb989c170e7e0e62fb672da06432e
Alejandro Di Stefano <addistefano@live.com.ar>
2024-09-16 21:34:56 -0300

    Clase 03

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,2 +1,27 @@
 console.log("Hola mundo desde TS");
 console.log("Log traducido desde TS a JS")
+
+let edad: number = 20;
+let nombre:string = "Alejandro";
+
+let isLoading: boolean = false;
+
+let precio: number | string = 10;
+let frutas: string[] = ["Banana", "Pera", "Manzana"];
+let frutas1: Array<string> = ["Banana", "Pera", "Manzana"]; // Otra forma de hacerlo
+let numerosYLetras: (string | number)[] = [1, "Ale"];
+let numerosYLetras1: Array<string | number> = [1, "Ale"]; // Otra forma de hacerlo
+let productos: object[] = [
+    {
+        id: 1,
+        nombre: "Azucar",
+        precio: 1080.50
+    }
+]
+
+function saludar(nombre: string){
+    console.log(`Hola ${nombre.toUpperCase()}!`);
+}
+
+saludar("Alejandro");
+
